7ded45584efb87deba037c287f5df7d58bc71326
iamtanmay <iamtanmay@gmail.com>
2023-05-31 12:43:46 +0200

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -1,4 +1,5 @@
-RAW_STR=$(awslocal ecr create-repository --repository-name test-ecr | grep "repositoryUri")
+ENDPOINT_URL=http://localhost:4566
+RAW_STR=$(aws --endpoint-url $ENDPOINT_URL ecr create-repository --repository-name test-ecr | grep "repositoryUri")
 RAW_STR=(${RAW_STR//:/})
 RAW_STR=${RAW_STR[1]}
 ECR_URI=$(echo ${RAW_STR}|sed 's/"//g'|sed 's/,//g')
