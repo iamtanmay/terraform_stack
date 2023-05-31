02e0c7967c8c1c91c9772bb53ca40a65020ad2a6
iamtanmay <iamtanmay@gmail.com>
2023-05-31 12:39:47 +0200

    Update serv_image_build.sh

--- serv_image_build.sh
+++ serv_image_build.sh
@@ -2,6 +2,7 @@ RAW_STR=$(awslocal ecr create-repository --repository-name test-ecr | grep "repo
 RAW_STR=(${RAW_STR//:/})
 RAW_STR=${RAW_STR[1]}
 ECR_URI=$(echo ${RAW_STR}|sed 's/"//g'|sed 's/,//g')
+cd ..
 git clone https://github.com/thedevs-network/kutt.git
 cd kutt
 docker build -t ECR_URI .
