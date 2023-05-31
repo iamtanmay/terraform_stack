63011ee951cf82c2fb23a9d888ce2628762d2949
iamtanmay <iamtanmay@gmail.com>
2023-05-31 14:07:57 +0200

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -1,8 +1,8 @@
 export RAW_STR=$(awslocal ecr create-repository --repository-name test-ecr | grep "repositoryUri")
 echo "$RAW_STR"
-RAW_STR=(${RAW_STR//:/})
-RAW_STR=${RAW_STR[1]}
-ECR_URI=$(echo ${RAW_STR}|sed 's/"//g'|sed 's/,//g')
+export RAW_STR=(${RAW_STR//:/})
+export RAW_STR=${RAW_STR[1]}
+export ECR_URI=$(echo ${RAW_STR}|sed 's/"//g'|sed 's/,//g')
 cd ..
 git clone https://github.com/thedevs-network/kutt.git
 cd kutt
