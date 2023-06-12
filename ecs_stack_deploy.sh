822e7c9e9f933ba7a7293f71bb1d361493bfca8f
iamtanmay <iamtanmay@gmail.com>
2023-06-12 04:33:15 +0200

    Update ecs_stack_deploy.sh

--- ecs_stack_deploy.sh
+++ ecs_stack_deploy.sh
@@ -1,4 +1,4 @@
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}_infra.yml --capabilities CAPABILITY_IAM
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
-aws cloudformation create-stack --stack-name $STACK_NAME"_SERVICE" --template-body file://templates/${STACK_NAME}_deploy.yml --parameters  ParameterKey=StackName,ParameterValue=$StackName ParameterKey=ImageUrl,ParameterValue=$ECR_URI:latest  --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name $STACK_NAME"-SERVICE" --template-body file://templates/${STACK_NAME}_deploy.yml --parameters  ParameterKey=StackName,ParameterValue=$StackName ParameterKey=ImageUrl,ParameterValue=$ECR_URI:latest  --capabilities CAPABILITY_IAM
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
