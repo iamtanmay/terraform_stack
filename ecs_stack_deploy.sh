08c47a7662281c0012d64c5b379afdec570a2e79
iamtanmay <iamtanmay@gmail.com>
2023-06-12 13:36:01 +0200

    Update ecs_stack_deploy.sh

--- ecs_stack_deploy.sh
+++ ecs_stack_deploy.sh
@@ -1,7 +1,19 @@
+#Infra
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}_infra.yml --capabilities CAPABILITY_IAM
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+
+#Redis
+aws cloudformation create-stack --stack-name $STACK_NAME"-Redis" --template-body file://templates/elasticache_redis.yml --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME"-Redis"
+
+#Postgres
+aws cloudformation create-stack --stack-name $STACK_NAME"-Postgres" --template-body file://templates/postgres.yml --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME"-Postgres"
+
+#Kutt
 aws cloudformation create-stack --stack-name $STACK_NAME"-SERVICE" --template-body file://templates/${STACK_NAME}_deploy.yml --parameters  ParameterKey=StackName,ParameterValue=$STACK_NAME ParameterKey=ImageUrl,ParameterValue=$ECR_URI:latest  --capabilities CAPABILITY_IAM
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME"-SERVICE"
+
 export CLUSTER_ARN=$(aws ecs list-clusters | jq -r '.clusterArns[0]')
 export TASK_ARN=$(aws ecs list-tasks --cluster $CLUSTER_ARN | jq -r '.taskArns[0]')
 aws ecs describe-tasks --cluster $CLUSTER_ARN --tasks $TASK_ARN | jq -r '.tasks[0].containers[0].networkBindings[0].hostPort'
