0d45abdb015b126e149a902086249405c47b1068
iamtanmay <iamtanmay@gmail.com>
2023-06-12 04:08:48 +0200

    Update ecs_stack_deploy.sh

--- ecs_stack_deploy.sh
+++ ecs_stack_deploy.sh
@@ -1,2 +1,3 @@
 aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}.yml
 aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}_deploy.yml --parameters ParameterKey=ImageUrl,ParameterValue=$ECR_URI:latest
