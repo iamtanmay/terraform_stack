9b87e77f4a0b35e75411cb9a269b8892e90844a9
iamtanmay <iamtanmay@gmail.com>
2023-06-12 03:52:35 +0200

    Update ecs_stack_deploy.sh

--- ecs_stack_deploy.sh
+++ ecs_stack_deploy.sh
@@ -1,2 +1,2 @@
-awslocal cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}.yml
-awslocal cloudformation wait stack-create-complete --stack-name $STACK_NAME
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://templates/${STACK_NAME}.yml
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
