ba3c725a335c895b52358b09f2d8c2bf3354e17e
iamtanmay <iamtanmay@gmail.com>
2023-06-12 04:07:47 +0200

    Update image_build.sh

--- image_build.sh
+++ image_build.sh
@@ -1,4 +1,4 @@
-export RAW_STR=$(aws ecr create-repository --repository-name test-ecr | grep "repositoryUri")
+export RAW_STR=$(aws ecr create-repository --repository-name $IMAGE_NAME | grep "repositoryUri")
 ECR_URI="$( cut -d ':' -f 2- <<< "$RAW_STR" )"
 export ECR_URI=$(echo ${ECR_URI}|sed 's/"//g'|sed 's/,//g')
 echo "New ECR created: $ECR_URI"
